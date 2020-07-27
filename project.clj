e73d4c3596c1d99b15e81f439a62c6cbe88924cc
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-07-27 22:52:29 +0200

    Add clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject kwrooijen/duct-drawbridge "0.1.0"
-  :description "Integrant library for Drawbridge"
+  :description "Duct library for Drawbridge"
   :url "https://github.com/kwrooijen/duct-drawbridge"
   :license {:name "MIT"}
   :dependencies [[nrepl/drawbridge "0.2.1"]
                  [integrant "0.8.0"]
-                 [ring-basic-authentication "1.0.5"]])
+                 [ring-basic-authentication "1.0.5"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :sign-releases false}]])
